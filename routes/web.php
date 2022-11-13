206d9d85a40935acb6653d3f451821cda4c9a82e
PietroDanieli <1pie7d@gmail.com>
2022-11-13 23:53:35 +0100

    add routeto admin.home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,4 +19,18 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// rotta generata da laravel
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// middleware è un software che fa da intermediario usando il buon Auth (che viene usato in tutti i file nella cartella Admin)
+Route::middleware('auth')
+    // namespace 'Admin': (controller inseriti in sottocatella Admin)
+    ->namespace('Admin')
+    // name: nome delle rotte che inizia con 'admin'
+    ->name('admin.')
+    // prefix: tutti gli url delle rotte che iniziano con 'admin'
+    ->prefix('admin')
+    // al group nel get '/' finiranno tutte le rotte davanti, potrebberò esserci più rotte, per questo il group
+    ->group(function(){
+        Route::get('/', 'HomeController@Index')->name('home');
+    });
