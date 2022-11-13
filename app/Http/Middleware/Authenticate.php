ef732f00a58807f7d587df064e527e88c9b2a4b9
PietroDanieli <1pie7d@gmail.com>
2022-11-14 00:27:40 +0100

    ....

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        // if (! $request->expectsJson()) {
             return route('login');
-        }
+        // }
     }
 }
