a6f1f1ca7f7bb0bedb731b7ccfffdc60af48be3a
PietroDanieli <1pie7d@gmail.com>
2022-11-17 09:09:20 +0100

    change name function in plural, is one array in the end...

--- app/Post.php
+++ app/Post.php
@@ -12,7 +12,7 @@ class Post extends Model
     public function category()  {
         return $this->belongsTo('App\Category');
     }
-    public function tag()  {
+    public function tags()  {
         return $this->belongsToMany('App\Tag');
     }
 }
