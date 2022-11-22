894e4e64966b0935c431a4f300f672a9c0df03ae
PietroDanieli <1pie7d@gmail.com>
2022-11-22 04:56:57 +0100

    import post and set in index the return of posts in json

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -15,6 +16,8 @@ class PostController extends Controller
     public function index()
     {
         //
+        $posts = Post::all();
+        return response()->json($posts);
     }
 
     /**
