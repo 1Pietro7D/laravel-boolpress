14ec724c768b589c03ae63650f2fd85e46aad04f
PietroDanieli <1pie7d@gmail.com>
2022-11-22 05:16:46 +0100

    import axios

--- public/js/front.js
+++ public/js/front.js
@@ -49730,7 +49730,7 @@ __webpack_require__.r(__webpack_exports__);
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
-
+window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
