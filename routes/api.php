bad9a09aa65e56d5f4f7b5bd8d4b8db5e579b335
PietroDanieli <1pie7d@gmail.com>
2022-11-23 08:54:35 +0100

    restrict route

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
  }); */
 
-Route::resource('posts', 'Api\PostController');
+Route::resource('posts', 'Api\PostController', ['only'=>['index','show']]);
 // andiamo a connetterci al controller CRUD associato ai posts
 // Route::get('/posts', 'Api\PostController@index');
 // questa rotta richiama il singolo index nel controller
