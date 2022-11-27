9975a8250b598a8a63d2d1a3acc12a67df4666ca
PietroDanieli <1pie7d@gmail.com>
2022-11-28 00:35:07 +0100

    cancel Post component

--- resources/js/components/PostListComponent.vue
+++ resources/js/components/PostListComponent.vue
@@ -5,15 +5,20 @@
     </div>
     <!-- controller page -->
     <div class="my-2">
-        <button :class="{ 'disable' : !postPageList.prev_page_url }" @click="goToPage(postPageList.prev_page_url)">Previous page</button>
+        <!-- previous btn -->
+        <button :class="{ 'disable' : !postPageList.prev_page_url }" @click="goToPage(postPageList.prev_page_url, currentPage - 1)">Previous page</button>
+        <!-- end previous btn -->
         <span>{{ currentPage }}/{{ totalPages }}</span>
-        <button :class="{ 'disable' : !postPageList.next_page_url }" @click="goToPage(postPageList.next_page_url)">Next page</button>
+        <!-- next btn -->
+        <button :class="{ 'disable' : !postPageList.next_page_url }" @click="goToPage(postPageList.next_page_url, currentPage + 1)">Next page</button>
+         <!-- end next btn -->
     </div>
   </div>
   <div v-else>Nothing post</div>
 </template>
 
 <script>
+
 export default {
     name: 'PostListComponent',
     // quando un dato deriva direttamente da un altro si usa il computed property per una questione di performance
@@ -26,7 +31,6 @@ export default {
             return this.postPageList.current_page;
         },
         totalPages(){
-            console.log(this.postPageList);
             return Math.ceil(this.postPageList.total / this.postPageList.per_page) ;
         }
      },
@@ -37,8 +41,12 @@ export default {
         showPost(id){
            this.$emit('clickedPost', id)
         },
-        goToPage(url){
-            this.$emit('requestPage', url)
+        goToPage(url, pageNumber){
+            if (url) {
+                this.$router.push({ path: '/posts', query: { page: pageNumber } })
+                this.$emit('requestPage', url)
+            }
+
         }
     }
 }
